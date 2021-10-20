2bf0647c9d303d0a6d860f1f9062eb11d49cd09f
Mateus Vitorino Guimarães <mateusvitorino@outlook.com.br>
2021-10-20 20:29:56 -0300

    db

--- src/logica/conexao.php
+++ src/logica/conexao.php
@@ -1,8 +1,8 @@
 <?php
-	$servidor = "localhost";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "teste";
+	$servidor = "us-cdbr-east-04.cleardb.com";
+	$usuario = "bb3bd2b935de69";
+	$senha = "fb7aa1ef";
+	$dbname = "heroku_fb1e780812f7b81";
 	
 	//Criar a conexao
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
